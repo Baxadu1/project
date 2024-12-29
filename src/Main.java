875b93fb2dc17229e8ad805b90614c2d99e119f2
baqyt kyanysh <bakyt.kuanysh6@gmail.com>
2024-12-29 21:27:58 +0500

    Created School class and its subclasses

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.sql.SQLOutput;
+public class Main {
+    public static void main(String[] args) {
+        Person p = new Person("Bakyt", "Kuanysh", 18, true);
+        System.out.println(p.toString());
+    }
+}
