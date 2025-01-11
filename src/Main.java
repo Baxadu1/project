2331472cefbe22bd0ef5222b843a75ed2674a1e6
baqyt kyanysh <bakyt.kuanysh6@gmail.com>
2025-01-11 18:35:33 +0500

    first

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.sql.SQLOutput;
-public class Main {
-    public static void main(String[] args) {
-        Person p = new Person("Bakyt", "Kuanysh", 18, true);
-        System.out.println(p.toString());
-    }
-}
